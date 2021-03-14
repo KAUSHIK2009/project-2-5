9269009da97bf03168e659771bf33d88529c2c3f
KAUSHIK2009 <77925814+KAUSHIK2009@users.noreply.github.com>
2021-03-14 07:02:34 +0530

    Add files via upload

---
+++ crumpledPapertemplate-main/crumpledPapertemplate-main/Paper.js
@@ -0,0 +1,24 @@
+class Paper {
+ constructor(x,y){
+     var options ={
+    isStatic : false,
+    restituiton : 0.3,
+    friction:0.5,
+    density:1.2
+     }
+    }
+}
+this.body=Bodiea.rectangle(x,y,50,50,optioms);
+this.width=50;
+this.height=50;
+
+world.add(world,this.body);
+
+display(){
+var pos=this.body.position;
+pos.x=mousex;
+pos.y=mousey;
+
+}
+
+
